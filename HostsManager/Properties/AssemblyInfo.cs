4f7ac8deee23c860e837f3363520e32d5c79486a
Bruno Pilgaard Pedersen <marduk.dk@gmail.com>
2015-03-05 01:27:07 +0100

    Added version 1.0.2 to assembly (same as chrome ext. version)

--- HostsManager/Properties/AssemblyInfo.cs
+++ HostsManager/Properties/AssemblyInfo.cs
@@ -22,14 +22,12 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6c074706-80bf-4da8-bd14-6cdbabffc50e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.2")]
+
+#if DEBUG
+[assembly: AssemblyInformationalVersion("1.0.2 DEBUG")]
+#else
+[assembly: AssemblyInformationalVersion("1.0.2 RELEASE")]
+#endif
